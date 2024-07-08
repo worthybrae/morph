cd86502d662081a5d4befbbc2277660fafc67d94
worthybrae <worthyrae@gmail.com>
2024-07-08 18:42:15 -0400

    updating ffmpeg output config

--- stream_processor.py
+++ stream_processor.py
@@ -180,10 +180,8 @@ def initialize_output_ffmpeg_process(width, height, fps):
         '-preset', 'ultrafast',
         '-tune', 'zerolatency',
         '-pix_fmt', 'yuv420p',
-
-        '-sc_threshold', '0',  # Disable scene change detection
         '-f', 'hls',
-        '-hls_list_size', '5',
+        '-hls_list_size', '10',
         '-hls_flags', 'delete_segments+append_list+omit_endlist',
         '-hls_segment_filename', '/tmp/hls/stream%03d.ts',
         '/tmp/hls/stream.m3u8'
