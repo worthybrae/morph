04b8be74a722e6796db178430a29eb9eedc745ae
worthybrae <worthyrae@gmail.com>
2024-07-14 16:16:29 -0400

    back to line tracing

---
+++ research/styling/dilation_eroding.py
@@ -0,0 +1,46 @@
+start = time.time()
+    gamma = 0.5  # Gamma value less than 1
+    inv_gamma = 1.0 / gamma
+    # Build a lookup table mapping pixel values [0, 255] to their adjusted gamma values
+    lookup_table = np.array([((i / 255.0) ** inv_gamma) * 255 for i in range(256)], dtype=np.uint8)
+    emphasized_darker = cv2.LUT(array, lookup_table) # Apply the gamma correction using the lookup table
+    times['make_darker'] = time.time() - start
+
+    start = time.time()
+    kernel_size = (27, 27)
+    # Create a random dilation kernel
+    kernel_dilate = np.array([
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], dtype=np.uint8)
+    # Create a random erosion kernel
+    kernel_erode = np.array([
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], dtype=np.uint8)
+
+    # Apply dilation with the time-based random kernel
+    dilated = cv2.dilate(emphasized_darker, kernel_dilate, iterations=1)
+
+    # Apply erosion with another time-based random kernel
+    eroded = cv2.erode(dilated, kernel_erode, iterations=1)
+
+    times['dilation_erosion'] = time.time() - start
--- stream/processor.py
+++ stream/processor.py
@@ -190,59 +190,24 @@ def process_frame(input_process, output_process, width, height):
     array = np.frombuffer(frame, dtype=np.uint8).reshape((height, width))
     times['convert_array'] = time.time() - start
 
-    # start = time.time()
-    # background_color, line_color = get_colors()
-    # times['get_colors'] = time.time() - start
+    start = time.time()
+    background_color, line_color = get_colors()
+    times['get_colors'] = time.time() - start
 
     start = time.time()
-    gamma = 0.5  # Gamma value less than 1
-    inv_gamma = 1.0 / gamma
-    # Build a lookup table mapping pixel values [0, 255] to their adjusted gamma values
-    lookup_table = np.array([((i / 255.0) ** inv_gamma) * 255 for i in range(256)], dtype=np.uint8)
-    emphasized_darker = cv2.LUT(array, lookup_table) # Apply the gamma correction using the lookup table
-    times['make_darker'] = time.time() - start
+    edges = cv2.Canny(frame, 300, 400, apertureSize=5)
+    times['get_edges'] = time.time() - start
 
     start = time.time()
-    kernel_size = (27, 27)
-    # Create a random dilation kernel
-    kernel_dilate = np.array([
-        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], dtype=np.uint8)
-    # Create a random erosion kernel
-    kernel_erode = np.array([
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], dtype=np.uint8)
-
-    # Apply dilation with the time-based random kernel
-    dilated = cv2.dilate(emphasized_darker, kernel_dilate, iterations=1)
-
-    # Apply erosion with another time-based random kernel
-    eroded = cv2.erode(dilated, kernel_erode, iterations=1)
-
-    times['dilation_erosion'] = time.time() - start
+    colored_frame = np.full((height, width, 3), background_color, dtype=np.uint8)
+    times['draw_background'] = time.time() - start
 
     start = time.time()
-    output_process.stdin.write(eroded.tobytes())
+    colored_frame[edges != 0] = line_color
+    times['draw_lines'] = time.time() - start
+
+    start = time.time()
+    output_process.stdin.write(colored_frame.tobytes())
     times['send_output'] = time.time() - start
 
     times['total'] = time.time() - total_start
